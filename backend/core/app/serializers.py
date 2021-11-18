b909b57413829568ae7b85d08a52f797b33834be
MagisterFelix <felix.vlad.14@gmail.com>
2021-11-19 01:51:45 +0200

    Implement JWT Authentication

---
+++ backend/core/app/serializers.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'time_zone')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=40)
+    password = serializers.CharField(max_length=40, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+        JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'No user with this email and password was found.')
+
+        payload = JWT_PAYLOAD_HANDLER(user)
+        jwt_token = JWT_ENCODE_HANDLER(payload)
+        update_last_login(None, user)
+        response = {
+            'email': user.email,
+            'token': jwt_token
+        }
+
+        return response
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email', 'name'
+        )
--- backend/core/app/views.py
+++ backend/core/app/views.py
@@ -1,3 +1,123 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from core.app.models import User
+from core.app.serializers import UserRegistrationSerializer, UserLoginSerializer, ProfileSerializer
+
+
+LOGIN_FAILS = {
+    'email': 'User must have an email.',
+    'password': 'User must have a password.'
+}
+REGISTRATION_FAILS = {
+    **LOGIN_FAILS,
+    'time_zone': 'Time zone is required.'
+}
+
+
+class UserRegistrationView(APIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        for key, value in REGISTRATION_FAILS.items():
+            if not request.data.get(key):
+                success = False
+                status_code = status.HTTP_400_BAD_REQUEST
+                message = value
+                break
+        else:
+            serializer = self.serializer_class(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                success = True
+                status_code = status.HTTP_201_CREATED
+                message = 'User registered successfully.'
+            else:
+                success = False
+                message = ''
+                for value in serializer.errors.values():
+                    message += value[0][:-1].capitalize() + '.'
+                status_code = status.HTTP_400_BAD_REQUEST
+
+        response = {
+            'success': success,
+            'status code': status_code,
+            'message': message,
+        }
+
+        return Response(response, status=status_code)
+
+
+class UserLoginView(APIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        for key, value in LOGIN_FAILS.items():
+            if not request.data.get(key):
+                success = False
+                status_code = status.HTTP_400_BAD_REQUEST
+                message = value
+                token = None
+                break
+        else:
+            serializer = self.serializer_class(data=request.data)
+
+            if serializer.is_valid():
+                success = True
+                status_code = status.HTTP_200_OK
+                message = 'User logged in successfully.'
+                token = serializer.data['token']
+            else:
+                success = False
+                message = ''
+                for value in serializer.errors.values():
+                    message += value[0][:-1].capitalize() + '.'
+                status_code = status.HTTP_404_NOT_FOUND
+                token = None
+
+        response = {
+            'success': success,
+            'status code': status_code,
+            'message': message,
+            'token': token
+        }
+
+        return Response(response, status=status_code)
+
+
+class UserProfileView(APIView):
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def get(self, request):
+        user = User.objects.get(email=request.user)
+
+        success = True
+        status_code = status.HTTP_200_OK
+        message = 'User profile received successfully.'
+        data = {
+            'id': user.id,
+            'email': user.email,
+            'name': user.name,
+            'image': user.image,
+            'premium':  bool(user.premium),
+            'time_zone': user.time_zone
+        }
+
+        response = {
+            'success': success,
+            'status code': status_code,
+            'message': message,
+            'data': data
+        }
+
+        return Response(response, status=status_code)
