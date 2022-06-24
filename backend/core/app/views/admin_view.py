82eb75092534861f0505613b32f500fe75d1349f
MagisterFelix <felix.vlad.14@gmail.com>
2022-06-24 23:13:24 +0300

    Restructure admin view

--- backend/core/app/views/admin_view.py
+++ backend/core/app/views/admin_view.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
+from django.db.models import OuterRef, Subquery
 from django.http import HttpResponse
 from wsgiref.util import FileWrapper
 
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from core.settings import DBBACKUP_STORAGE_OPTIONS
@@ -29,11 +30,31 @@ class AdminView(APIView):
 
     def get(self, request):
         if request.headers.get('command') is None:
-            users = User.objects.all().values()
-            tasks = Task.objects.all().values()
-            subtasks = Subtask.objects.all().values()
-            tags = Tag.objects.all().values()
-            extras = Extra.objects.all().values()
+            users = User.objects.all().order_by('id').values()
+            tasks = Task.objects.all().annotate(
+                owner_name=Subquery(
+                    User.objects.filter(
+                        id=OuterRef('owner_id')
+                    ).values('email')
+                )).order_by('owner', 'date').values()
+            subtasks = Subtask.objects.all().annotate(
+                task_title=Subquery(
+                    Task.objects.filter(
+                        id=OuterRef('task_id')
+                    ).values('title')
+                )).order_by('task', 'title').values()
+            tags = Tag.objects.all().annotate(
+                task_title=Subquery(
+                    Task.objects.filter(
+                        id=OuterRef('task_id')
+                    ).values('title')
+                )).order_by('task', 'title').values()
+            extras = Extra.objects.all().annotate(
+                task_title=Subquery(
+                    Task.objects.filter(
+                        id=OuterRef('task_id')
+                    ).values('title')
+                )).order_by('task', 'information').values()
 
             success = True
             status_code = status.HTTP_200_OK
@@ -69,6 +90,7 @@ class AdminView(APIView):
 
                 with open(file_path, 'rb') as file:
                     response = HttpResponse(FileWrapper(file), content_type='application/psql', status=status_code)
+                    response["Access-Control-Expose-Headers"] = "Content-Disposition"
                     response['Content-Disposition'] = f'attachment; filename="{file_name}"'
 
                 return response
@@ -78,45 +100,6 @@ class AdminView(APIView):
                 message = function_response
 
                 return Response(response, status=status_code)
-
-    def post(self, request):
-        serializer = self.serializer_classes[request.data['model']](data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            success = True
-            status_code = status.HTTP_201_CREATED
-            message = 'Info created successfully.'
-        else:
-            success = False
-            message = ''
-            for value in serializer.errors.values():
-                message += value[0][:-1].capitalize() + '.'
-            status_code = status.HTTP_400_BAD_REQUEST
-
-        response = {
-            'success': success,
-            'status_code': status_code,
-            'message': message
-        }
-
-        return Response(response, status=status_code)
-
-    def put(self, request):
-        if request.headers.get('command') is None:
-            serializer = self.serializer_classes[request.data['model']](data=request.data)
-
-            if serializer.is_valid():
-                serializer.save()
-                success = True
-                status_code = status.HTTP_200_OK
-                message = 'Info updated successfully.'
-            else:
-                success = False
-                message = ''
-                for value in serializer.errors.values():
-                    message += value[0][:-1].capitalize() + '.'
-                status_code = status.HTTP_400_BAD_REQUEST
         else:
             command, file_name = request.headers['command'].split('/')
 
@@ -131,6 +114,64 @@ class AdminView(APIView):
                     success = False
                     status_code = status.HTTP_404_NOT_FOUND
                     message = 'No file with this name was found.'
+
+                response = {
+                    'success': success,
+                    'status_code': status_code,
+                    'message': message
+                }
+
+            return Response(response, status=status_code)
+
+    def post(self, request):
+        serializer = self.serializer_classes[request.data['model']](data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            success = True
+            status_code = status.HTTP_201_CREATED
+            message = 'Info created successfully.'
+        else:
+            success = False
+            message = ''
+            print(serializer.errors)
+            for value in serializer.errors.values():
+                message += value[0][:-1].capitalize() + '.'
+            status_code = status.HTTP_400_BAD_REQUEST
+
+        response = {
+            'success': success,
+            'status_code': status_code,
+            'message': message
+        }
+
+        return Response(response, status=status_code)
+
+    def put(self, request):
+        if request.data['model'] == 'user':
+            model = User.objects.filter(id=request.data['id'])
+        if request.data['model'] == 'task':
+            model = Task.objects.filter(id=request.data['id'])
+        if request.data['model'] == 'subtask':
+            model = Subtask.objects.filter(id=request.data['id'])
+        if request.data['model'] == 'tag':
+            model = Tag.objects.filter(id=request.data['id'])
+        if request.data['model'] == 'extra':
+            model = Extra.objects.filter(id=request.data['id'])
+
+        serializer = self.serializer_classes[request.data['model']](model.first(), data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            success = True
+            status_code = status.HTTP_200_OK
+            message = 'Info updated successfully.'
+        else:
+            success = False
+            message = ''
+            for value in serializer.errors.values():
+                message += value[0][:-1].capitalize() + '.'
+            status_code = status.HTTP_400_BAD_REQUEST
 
         response = {
             'success': success,
