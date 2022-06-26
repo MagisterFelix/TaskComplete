bed0679c6549dd1f703410d27e1f9b313f75dd4a
MagisterFelix <felix.vlad.14@gmail.com>
2022-06-27 00:43:58 +0300

    Update base url

--- mobile/app/src/main/java/com/android/mobile/core/Constants.kt
+++ mobile/app/src/main/java/com/android/mobile/core/Constants.kt
@@ -1,7 +1,7 @@
 package com.android.mobile.core
 
 object Constants {
-    const val BASE_URL = "http://10.0.2.2:8000"
+    const val BASE_URL = "https://10.0.2.2:8000"
     const val REGISTER_URL = "/signUp"
     const val LOGIN_URL = "/signIn"
     const val PROFILE_URL = "/profile"
