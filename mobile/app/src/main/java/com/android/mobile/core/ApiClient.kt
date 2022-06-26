b47d29f83eaff8f095c398c27be1da2ebf3f007b
MagisterFelix <felix.vlad.14@gmail.com>
2022-06-27 02:17:37 +0300

    Update ApiClient

--- mobile/app/src/main/java/com/android/mobile/core/ApiClient.kt
+++ mobile/app/src/main/java/com/android/mobile/core/ApiClient.kt
@@ -1,16 +1,55 @@
 package com.android.mobile.core
 
+import android.annotation.SuppressLint
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
+import javax.security.cert.CertificateException
+
 
 class ApiClient {
     private lateinit var apiService: ApiService
 
+    private fun getUnsafeOkHttpClient(): OkHttpClient.Builder =
+        try {
+            val trustAllCerts: Array<TrustManager> = arrayOf(
+                @SuppressLint("CustomX509TrustManager")
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(chain: Array<X509Certificate?>?,
+                                                    authType: String?) = Unit
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(chain: Array<X509Certificate?>?,
+                                                    authType: String?) = Unit
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+            )
+            val sslContext: SSLContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+            val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory,
+                trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier { _, _ -> true }
+            builder
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
+    private fun getOkHttpBuilder(): OkHttpClient.Builder = getUnsafeOkHttpClient()
+
     fun getApiService(): ApiService {
 
         if (!::apiService.isInitialized) {
+
             val retrofit = Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
+                .client(getOkHttpBuilder().build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
